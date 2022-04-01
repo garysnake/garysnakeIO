1830121b2e7832ec571578546351bf26c2f87c35
garysnake <garysnake@garys-mbp.lan>
2022-04-01 10:10:56 -0500

    Add Sanity Next JS into the project

---
+++ README.md
@@ -0,0 +1,4 @@
+** This is the mind palace of the one and only Garysnake. Everyone is welcome here. **
+
+Well this is just my personal blog
+
---
+++ web/pages/index.js
@@ -0,0 +1,12 @@
+// index.js
+const Index = () => {
+    return (
+      <div>
+        <p>Hello world!</p>
+      </div>
+    )
+}
+
+export default Index;
+
+
